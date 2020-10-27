307cf541cfabd0760b40c6335f6607a94e287f01
Diana2020088 <62649363+Diana2020088@users.noreply.github.com>
2020-10-27 22:35:41 +0000

    first

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 
 const projectsController = require("./controllers/projects")();
 const usersController = require("./controllers/users")();
-const issuesController = require("./controller/issues")();
+//const issuesController = require("./controller/issues")();
 
 const app = (module.exports = express());
 
@@ -33,7 +33,7 @@ app.get("/projects", projectsController.postController);
 //A Project
 app.get("/projects/:slug", projectsController.getBySlug);
 //all issues
-app.get("/projects/:slug/issues", issuesController.getController);
+//app.get("/projects/:slug/issues", issuesController.getController);
 
 
 app.listen(port, hostname, () => {
