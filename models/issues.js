0604824952d9ee2f3a3f4c4d8112c2946127482a
Diana2020088 <62649363+Diana2020088@users.noreply.github.com>
2020-10-29 14:38:03 +0000

    testing comments

--- models/issues.js
+++ models/issues.js
@@ -14,7 +14,7 @@ module.exports = () =>{
     }
 
     const findComments = async () => {
-        const comments = await db.COLLECTION.find(comm);
+        const comments = await db.collection(COLLECTION).find(comm);
         return comments;
     }
 
