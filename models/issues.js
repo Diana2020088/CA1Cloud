1168d1bcdc4938a5bb30fd4ddbb2765e8e0c2209
Diana2020088 <62649363+Diana2020088@users.noreply.github.com>
2020-10-29 16:14:41 +0000

    testing comments

--- models/issues.js
+++ models/issues.js
@@ -1,6 +1,6 @@
 const db = require("../db")();
 const COLLECTION = "issues";
-const comm = [{}, {comments:1}]; 
+const comm = {title:1}; 
 
 module.exports = () =>{
     const get = async (issueNumber = null) =>{
