ac8028645cb0318ee86c0a8263eb8506a6831375
Diana2020088 <62649363+Diana2020088@users.noreply.github.com>
2020-10-29 16:07:23 +0000

    testing comments

--- controllers/issues.js
+++ controllers/issues.js
@@ -10,7 +10,7 @@ module.exports = () => {
     }
 
     const getComments = async (req, res) => {
-        res.json(await issues.find());
+        res.json(await issues.findComments());
     }
 
     const postController = async (req, res) => {
