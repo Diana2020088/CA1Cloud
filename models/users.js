e997f9065ced1853dea6d942e07151c426e72dd0
Diana2020088 <62649363+Diana2020088@users.noreply.github.com>
2020-10-31 19:40:51 +0000

    comments post

--- models/users.js
+++ models/users.js
@@ -2,18 +2,34 @@ const db = require("../db")();
 const COLLECTION = "users";
 
 module.exports = () => {
+
+    const getByKey = async (key) => {
+        if (!key) {
+            console.log(" 01: Missing key");
+            return null;
+        }
+        const users = await db.get(COLLECTION, {key});
+        if (users.length !== 1) {
+            console.log(" 02: Bad key");
+            return null;
+        }
+        return users[0];
+    };
+
     const get = async (email = null) => {
         console.log('inside users model');
-        if(!email) {
+        if (!email) {
             const users = await db.get(COLLECTION);
             return users;
         }
 
-        const user = await db.get(COLLECTION, { email });
+        const user = await db.get(COLLECTION, {
+            email
+        });
         return user;
     };
 
-    const add =async (name, email, usertype) => {
+    const add = async (name, email, usertype) => {
         const results = await db.add(COLLECTION, {
             name: name,
             email: email,
@@ -24,5 +40,6 @@ module.exports = () => {
     return {
         get,
         add,
+        getByKey
     }
 }
